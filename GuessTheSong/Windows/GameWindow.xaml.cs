cad6d64b0684f2aa2472b09ae03c8ab70b6be858
eciseeyou <eciseeyou@gmail.com>
2016-04-23 00:10:31 +0300

    simple console commands added

--- GuessTheSong/Windows/GameWindow.xaml.cs
+++ GuessTheSong/Windows/GameWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -65,6 +66,7 @@ namespace GuessTheSong.Windows
             WinnerCombobox.IsEnabled = false;
             AddPoints.IsEnabled = false;
             RemovePoints.IsEnabled = false;
+            _currentSelectionButton.Background = new SolidColorBrush(Colors.PaleGreen);
             ShowSongInfo(sender, e);
         }
 
@@ -82,5 +84,64 @@ namespace GuessTheSong.Windows
 
             _viewModel.PauseCommand.Execute(null);
         }
+
+        #region consoleCommands
+
+        private int? ParseNullableInt(string value)
+        {
+            int result;
+            var parsed = int.TryParse(value, out result);
+            return parsed ? result : (int?)null;
+        }
+
+        private Song GetSongByCategoryAndPrice(int categoryNo, int price)
+        {
+            return  _viewModel.GameData[categoryNo - 1].Songs.FirstOrDefault(x => x.Price == price) ?? new Song();
+        }
+
+        public GameParticipant GetParticipantByNo(int participantNo)
+        {
+            return _viewModel.Participants.OrderByDescending(x => x.Score).ToList()[participantNo - 1];
+        }
+
+        private void ProcessConsoleCommandClick(object sender, RoutedEventArgs e)
+        {
+            var command = ConsoleTextBox.Text;
+            if (string.IsNullOrEmpty(command)) return;
+
+            var variables = command.Split(' ');
+            if (variables.Length < 3) return;
+
+            var categoryNoOrParticipantNo = ParseNullableInt(variables[1]);
+            var pointsOrPrice = ParseNullableInt(variables[2]);
+
+            if (categoryNoOrParticipantNo == null || pointsOrPrice == null) return;
+
+            switch (variables[0].ToUpper())
+            {
+                case "ADD":
+                    if (_viewModel.Participants.Count > categoryNoOrParticipantNo - 1)
+                       GetParticipantByNo(categoryNoOrParticipantNo.Value).Score += pointsOrPrice.Value;
+                    break;
+                case "REMOVE":
+                    if (_viewModel.Participants.Count > categoryNoOrParticipantNo - 1)
+                        GetParticipantByNo(categoryNoOrParticipantNo.Value).Score -= pointsOrPrice.Value;
+                    break;
+                case "SET":
+                    if (_viewModel.Participants.Count > categoryNoOrParticipantNo - 1)
+                        GetParticipantByNo(categoryNoOrParticipantNo.Value).Score = pointsOrPrice.Value;
+                    break;
+                case "LOCK":
+                    if (_viewModel.GameData.Count > categoryNoOrParticipantNo - 1)
+                        GetSongByCategoryAndPrice(categoryNoOrParticipantNo.Value, pointsOrPrice.Value).IsGuessed = true;
+                    break;
+                case "UNLOCK":
+                    if (_viewModel.GameData.Count > categoryNoOrParticipantNo - 1)
+                        GetSongByCategoryAndPrice(categoryNoOrParticipantNo.Value, pointsOrPrice.Value).IsGuessed = false;
+                    break;
+            }
+        }
+
+        #endregion
     }
 }
